89f12b1de67220262b2f3ebd4cafeb06da149bd0
Khalid Waleed Alsulaiman <alsulaiman.khalid@gmail.com>
2019-11-27 10:55:22 +0500

    make G as greater in filter

--- bikeshare.py
+++ bikeshare.py
@@ -31,7 +31,7 @@ def get_filters():
             continue
         break
 
-    # get the filter
+    # Get the filter
     print('\nData can be filtered by:\n- Month\n- Day\n- None')
 
     while True:
